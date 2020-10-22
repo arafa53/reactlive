ada727176084a7c3056baa166d2cd68737fc24ae
Arafa islam <ajjhalak@gmail.com>
2020-10-23 04:07:37 +0600

    Updates

---
+++ precache-manifest.4555e0dc70f0fc3ad6a7b549c4868592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "547c157bccc4c490ac53b81b70201cce",
+    "url": "/reactlive/index.html"
+  },
+  {
+    "revision": "6acb724f08740f31ad69",
+    "url": "/reactlive/static/css/main.45e7c3c5.chunk.css"
+  },
+  {
+    "revision": "c91652d9a343828d3b9a",
+    "url": "/reactlive/static/js/2.193a218b.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/reactlive/static/js/2.193a218b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6acb724f08740f31ad69",
+    "url": "/reactlive/static/js/main.149eafdf.chunk.js"
+  },
+  {
+    "revision": "e21267082983eaf41c47",
+    "url": "/reactlive/static/js/runtime-main.62aad2bb.js"
+  }
+]);
